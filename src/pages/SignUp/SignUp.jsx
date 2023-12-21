ba76451b6086c91d29bbb3c4fefb51a7ae1f7ce1
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-12-22 05:45:02 +0600

    added createUser functionality

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,16 +1,40 @@
+import useAuth from "../../hooks/useAuth";
+
 const SignUp = () => {
+  const { createUser } = useAuth();
+
+  const handleSignUpForm = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, photo, email, password);
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content container flex-col py-20">
         <h2 className="font-bold text-4xl mb-12">Sign Up Please!</h2>
         <div className="card w-3/4 shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handleSignUpForm} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
               </label>
               <input
                 type="text"
+                name="name"
                 placeholder="Name"
                 className="input input-bordered"
                 required
@@ -22,6 +46,7 @@ const SignUp = () => {
               </label>
               <input
                 type="text"
+                name="photo"
                 placeholder="Photo URL"
                 className="input input-bordered"
                 required
@@ -33,6 +58,7 @@ const SignUp = () => {
               </label>
               <input
                 type="email"
+                name="email"
                 placeholder="Email"
                 className="input input-bordered"
                 required
@@ -44,6 +70,7 @@ const SignUp = () => {
               </label>
               <input
                 type="password"
+                name="password"
                 placeholder="Password"
                 className="input input-bordered"
                 required
